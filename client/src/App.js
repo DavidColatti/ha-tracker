1dcdbf737d9271dddb40746b0a13500121218eaa
David Colatti <davidcolatti@gmail.com>
2020-08-27 11:27:58 -0400

    feat: initial commit and set up

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,28 @@
 import React from "react";
 
+import Table from "./components/Table";
+
+const data = [
+  {
+    spid: "2201920",
+    dateLastSeen: new Date("2020", 1, 10).toLocaleDateString(),
+  },
+  {
+    spid: "8219207",
+    dateLastSeen: "",
+  },
+  {
+    spid: "2201123",
+    dateLastSeen: new Date("2020", 3, 1).toLocaleDateString(),
+  },
+];
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Table data={data} />
+    </div>
+  );
 };
 
 export default App;
---
+++ client/src/components/Table/Table.jsx
@@ -0,0 +1,82 @@
+import React from "react";
+import { MDBDataTable } from "mdbreact";
+import { useState } from "react";
+import { useEffect } from "react";
+import moment from "moment";
+moment().format();
+
+const Table = ({ data }) => {
+  const [rows, setRows] = useState([]);
+
+  useEffect(() => {
+    const sevenDaysOut = (date) => {
+      if (date) {
+        return moment(date).add(7, "days").calendar();
+      } else return "";
+    };
+
+    if (data) {
+      const newData = data.map((each) => ({
+        spid: each.spid,
+        notesLink: <a href={`http://www.noteslink.com${each.spid}`}>Notes</a>,
+        bettiLink: <a href={`http://www.bettilink.com${each.spid}`}>Link</a>,
+        dateLastSeen: <input value={each.dateLastSeen} />,
+        prospectDate: sevenDaysOut(each.dateLastSeen),
+        deleteBtn: (
+          <i onClick={() => console.log(`deleting spid ${each.spid}`)}>X</i>
+        ),
+      }));
+
+      setRows(newData);
+    }
+  }, [data]);
+
+  const tableData = {
+    columns: [
+      {
+        label: "SPID",
+        field: "spid",
+        width: 150,
+      },
+      {
+        label: "Notes",
+        field: "notesLink",
+        width: 270,
+      },
+      {
+        label: "Betti Link",
+        field: "bettiLink",
+        width: 200,
+      },
+      {
+        label: "Date Last Seen",
+        field: "dateLastSeen",
+        width: 100,
+      },
+      {
+        label: "Prospect Date",
+        field: "prospectDate",
+        width: 150,
+      },
+      {
+        label: "Delete",
+        field: "deleteBtn",
+        width: 150,
+      },
+    ],
+    rows: rows,
+  };
+
+  return (
+    <MDBDataTable
+      small
+      hover
+      striped
+      data={tableData}
+      paging={false}
+      order={["prospectDate", "desc"]}
+    />
+  );
+};
+
+export default Table;
